44dba7f5659afc3cf76f5b7c8c9fad1c4864cb79
Jordanwu <w99jordan@gmail.com>
2022-03-30 18:36:13 +0800

    Revised week1 assignment: Basic: 4, 7 Function: 2

---
+++ week2/week2.playground/Pages/Enumerations and Optionals in Swift.xcplaygroundpage/Contents.swift
@@ -0,0 +1,5 @@
+1. There are several gasoline types, 92, 95, 98, and diesel that we can use enum to model them.
+    ● Please create an enum named Gasoline to model gasoline.
+    ● Every kind of gasoline has its price. Please create a method named getPrice in Gasoline enum that will return different prices depending on different gasoline.
+    ● Please establish for   . The data type of raw value should raw values Gasoline be String. For example,
+    ● Please explain what enum associated value is and how it works.
---
+++ week2/week2.playground/Pages/Object-Oriented Swift.xcplaygroundpage/Contents.swift
@@ -0,0 +1,4 @@
+//1. Declare a class Animal with property gender and method eat(). The data type of gender should be enum Gender as below and when you call eat() method, it will print
+//    I eat everything!
+
+
