8d84f9b67e0f7a4a8eb182bd9677457d97d23722
Jordanwu <w99jordan@gmail.com>
2022-03-31 11:03:43 +0800

    revised 4, 7

--- week2/week2.playground/Pages/Object-Oriented Swift.xcplaygroundpage/Contents.swift
+++ week2/week2.playground/Pages/Object-Oriented Swift.xcplaygroundpage/Contents.swift
@@ -1,4 +1,42 @@
-//1. Declare a class Animal with property gender and method eat(). The data type of gender should be enum Gender as below and when you call eat() method, it will print
-//    I eat everything!
+//1. Declare a class Animal with property gender and method eat(). The data type of gender should be enum Gender as below and when you call eat() method, it will print I eat everything!
+
+class Animal {
+    enum Gender: String {
+        case male
+        case female
+        case undefined
+    }
+    var gender: Gender = .male
+func eat(){
+            print("I eat everything!")
+        }
+}
+
+//2. Declare three classes: Elephant, Tiger, Horse that inherits from Animal and override the eat method to print what they usually eat.
+    
 
 
+
+
+
+//4. What’s the difference between Struct and Class ?
+//Struct is value Type
+
+
+
+//5. What’s the difference between instance method and type method ?
+
+
+
+
+//6. What does Initilizer do in class and struct ?
+
+
+
+
+//7. What does self mean in an instance method and a type method ?
+
+
+
+
+//8. What’s the difference between reference type and value type ?
